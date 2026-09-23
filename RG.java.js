9d9b717495215d02aa3e046d6c1fd335961079ff
muriloanjo5000-cell <muriloanjo5000@gmail.com>
2026-09-22 21:56:09 -0300

    RG.java.js

---
+++ RG.java.js
@@ -0,0 +1,12 @@
+@Entity
+@Table(name = "alunos").
+public
+
+let nullable;
+<div className="form-group">
+    <label htmlFor="rg">RG:</label>
+    <input type="text" id="rg" className="form-control" required/>
+</div>
+
+let FROM;
+SELECT * FROMalunos;
